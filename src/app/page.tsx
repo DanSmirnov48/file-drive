629c02305be5ac5de418d1e966fc959139e78640
Dan <daniel.smirnov17@gmail.com>
2024-03-02 00:46:55 +0000

    added auth based on org and tokenIdentifier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,35 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const files = useQuery(api.files.getFiles)
+
+  const { organization, isLoaded: isOrgLoaded } = useOrganization()
+  const { isLoaded: userLoaded, user } = useUser()
+
+  let orgId = null
+
+  if (isOrgLoaded && userLoaded) {
+    orgId = organization?.id ?? user?.id
+  }
+
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip")
   const createFile = useMutation(api.files.createFile)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignedIn>
-        <SignOutButton>
-          <Button>Sign Out</Button>
-        </SignOutButton>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
       {files?.map(file => {
         return <div key={file._id}>{file.name}</div>
       })}
 
       <Button onClick={() => {
+        if (!orgId) return
         createFile({
           name: `test file ${Math.random()}`,
+          orgId
         })
       }}>Click me</Button>
     </main>
