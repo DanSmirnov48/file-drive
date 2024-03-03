17e377e423916536f47ce66893db6e803368c014
Dan <daniel.smirnov17@gmail.com>
2024-03-03 00:37:40 +0000

    added file preview in card & download fiels

--- convex/files.ts
+++ convex/files.ts
@@ -1,6 +1,7 @@
 import { ConvexError, v } from "convex/values";
 import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server";
 import { getUser } from "./users";
+import { fileTypes } from "./schema";
 
 export const generateUploadUrl = mutation(async (ctx) => {
     const identity = await ctx.auth.getUserIdentity();
@@ -26,6 +27,7 @@ export const createFile = mutation({
         name: v.string(),
         fileId: v.id("_storage"),
         orgId: v.string(),
+        type: fileTypes,
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
@@ -43,7 +45,8 @@ export const createFile = mutation({
         await ctx.db.insert("files", {
             name: args.name,
             orgId: args.orgId,
-            fileId: args.fileId
+            fileId: args.fileId,
+            type: args.type,
         });
     },
 });
